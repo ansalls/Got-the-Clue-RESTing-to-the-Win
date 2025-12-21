d13f06c6457f63a4d9b0677c664336824d49bad6
Tony <anthonysalls@gmail.com>
2025-12-21 15:41:10 +0000

    Fix auth validation and vote input

--- app/routers/user.py
+++ app/routers/user.py
@@ -19,6 +19,12 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     hashed_password = utils.hash(user.password)
     user.password = hashed_password
 
+    existing_user = db.query(models.User).filter(
+        models.User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT,
+                            detail="Email already registered")
+
     new_user = models.User(**user.dict())
     db.add(new_user)
     db.commit()
